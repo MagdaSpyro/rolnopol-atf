36027fa94ffa294ce8ef06e5cb4bdca136c6b265
Magda Sitek <mie@spyro-soft.com>
2026-04-09 18:24:47 +0200

    Add smoke test for page title

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("/");
+test("should displayed the correct page title", async ({ page }) => {
+  await page.goto("");
   await expect(page).toHaveTitle(/Rolnopol/);
 });
 
