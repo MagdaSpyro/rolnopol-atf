5cbd18525c0610e61d953f90f1b91b8f33e91336
Magda Sitek <mie@spyro-soft.com>
2026-04-16 12:47:21 +0200

    Add BasePage class documentation comment

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -1,6 +1,6 @@
 import { Page } from "@playwright/test";
 import { PageUrl } from "../constants/pageUrls";
-
+//BasePage to klasa abstrakcyjna, która definiuje wspólne właściwości i metody dla wszystkich stron w aplikacji. Każda konkretna strona (np. HomePage, LoginPage) będzie dziedziczyć po BasePage i implementować swoją własną wartość url oraz ewentualnie dodatkowe metody specyficzne dla danej strony. Metoda goto() umożliwia nawigację do określonej strony, korzystając z jej url.
 export abstract class BasePage {
   protected abstract readonly url: PageUrl;
   readonly page: Page;
