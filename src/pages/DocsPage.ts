dd6bfa6aeb8cc3aa22515a81ae2a537cc6860f73
Magda Sitek <mie@spyro-soft.com>
2026-04-30 13:56:00 +0200

    login tests created by agent

--- src/pages/DocsPage.ts
+++ src/pages/DocsPage.ts
@@ -3,7 +3,7 @@ import { PAGE_URLS } from "../constants/pageUrls";
 import { BasePage } from "./BasePage";
 
 export class DocsPage extends BasePage {
-  protected readonly PAGE_URL = PAGE_URLS.DOCS;
+  readonly PAGE_URL = PAGE_URLS.DOCS;
   readonly docsHeaderSubtitle: Locator;
 
   constructor(page: Page) {
